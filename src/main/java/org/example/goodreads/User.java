7a15978a1bf332cabf09ddd66e8a6892d8e9978b
mkopczewski <kopczewski.michal101@gmail.com>
2024-12-17 23:50:07 +0100

    Added User class

---
+++ src/main/java/org/example/goodreads/User.java
@@ -0,0 +1,36 @@
+package org.example.goodreads;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Builder
+@ToString(includeFieldNames = true)
+@Table(name = "user")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+    private String name;
+    private String surname;
+    @Column(nullable = false)
+    private String passwordHash;
+    @Column(nullable = false)
+    private String email;
+    private String description;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    @Transient
+    private static final ObjectMapper mapper = new ObjectMapper();
+}
