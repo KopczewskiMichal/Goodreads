0615a74a6110f78f96dbd45753de8b49bb7ad34f
mkopczewski <kopczewski.michal101@gmail.com>
2025-01-02 21:27:10 +0100

    Application starts but on login throws exception

--- src/main/java/org/example/goodreads/GoodreadsApplication.java
+++ src/main/java/org/example/goodreads/GoodreadsApplication.java
@@ -3,12 +3,14 @@ package org.example.goodreads;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.example")
 public class GoodreadsApplication {
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure().load();
