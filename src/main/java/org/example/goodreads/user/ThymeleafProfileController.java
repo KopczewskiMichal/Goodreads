bbb8052e911c12f6c7c26b5a4b4887700688e3b0
mkopczewski <kopczewski.michal101@gmail.com>
2025-01-13 23:58:31 +0100

    Usuwanie użytkownika przekierowuje do głównej ale nie usuwa konta

--- src/main/java/org/example/goodreads/user/ThymeleafProfileController.java
+++ src/main/java/org/example/goodreads/user/ThymeleafProfileController.java
@@ -3,10 +3,16 @@ package org.example.goodreads.user;
 import jakarta.servlet.http.HttpServletRequest;
 import org.example.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.NoSuchElementException;
 
 @Controller
 @RequestMapping("/profile")
@@ -30,4 +36,23 @@ public class ThymeleafProfileController {
         model.addAttribute("user", user);
         return "userPage";
     }
+
+
+    @PostMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") long userId, RedirectAttributes redirectAttributes) {
+        try {
+            userService.deleteUser(userId);
+            redirectAttributes.addFlashAttribute("message", "User deleted successfully.");
+            return "redirect:/logout";
+        } catch (NoSuchElementException e) {
+            redirectAttributes.addFlashAttribute("error", "User not found.");
+            return "redirect:/logout";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "An error occurred while deleting the user.");
+            return "redirect:/logout";
+        }
+    }
+
+
+
 }
