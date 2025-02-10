c27026448815731ebfa0b36e966cfa22c6e6f773
mkopczewski <kopczewski.michal101@gmail.com>
2025-02-10 16:29:26 +0100

    Formularz logowania, brak logiki logowania

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private formBuilder = inject(FormBuilder);
+  public loginForm!: FormGroup;
+  public message = "";
+  public ngOnInit(): void {
+    this.loginForm = this.formBuilder.nonNullable.group({
+      identifier: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
+  public onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      this.message = "Please fill all fields";
+    }
+  }
 }
